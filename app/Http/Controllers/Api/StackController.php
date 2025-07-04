b375c2b5892d93ddc267462799a8284303d81a23
unknown <mdtareq.me@gmail.com>
2025-07-05 01:34:04 +0600

    Stack Controller added

---
+++ app/Http/Controllers/Api/StackController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStackRequest;
+use App\Http\Requests\UpdateStackRequest;
+use App\Models\Stack;
+
+class StackController extends Controller
+{
+    public function index()
+    {
+        return Stack::all();
+    }
+
+    public function show($id)
+    {
+        return Stack::findOrFail($id);
+    }
+
+    // Admin methods
+    public function store(StoreStackRequest $request)
+    {
+        $stack = Stack::create($request->validated());
+        return response()->json($stack, 201);
+    }
+
+    public function update(UpdateStackRequest $request, $id)
+    {
+        $stack = Stack::findOrFail($id);
+        $stack->update($request->validated());
+        return $stack;
+    }
+
+    public function destroy($id)
+    {
+        Stack::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
