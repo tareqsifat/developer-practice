fc958795079cd78d72250f72251b295b8fd8f0b1
unknown <mdtareq.me@gmail.com>
2025-07-10 01:37:06 +0600

    Store Exam Request added

---
+++ app/Http/Requests/StoreExamRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreExamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'stack_id' => 'required|exists:stacks,id',
+            'subject_id' => 'nullable|exists:subjects,id',
+            'topic_id' => 'nullable|exists:topics,id',
+            'duration_minutes' => 'required|integer|min:1',
+            'total_marks' => 'required|integer|min:1',
+            'passing_score' => 'required|integer|min:0',
+            'max_attempts' => 'required|integer|min:1',
+            'is_active' => 'boolean',
+            'price_in_points' => 'integer|min:0',
+            'start_time' => 'nullable|date',
+            'end_time' => 'nullable|date|after:start_time'
+        ];
+    }
+}
