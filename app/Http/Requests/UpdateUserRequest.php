a48422c8c0f038ce7fd56efc6fd4e33aec7e14d9
unknown <mdtareq.me@gmail.com>
2025-07-18 01:40:24 +0600

    Update User Request added

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|email|unique:users,email,' . $this->user()->id,
+            'bio' => 'nullable|string',
+            'github_username' => 'nullable|string|max:50',
+            'linkedin_url' => 'nullable|url',
+            'website_url' => 'nullable|url'
+        ];
+    }
+}
