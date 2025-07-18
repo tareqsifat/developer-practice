397bf82802db914c0e62e43ad6ca5b16583571ce
unknown <mdtareq.me@gmail.com>
2025-07-19 01:41:10 +0600

    Blog Comment model added

---
+++ app/Models/BlogComment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogComment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'blog_post_id',
+        'user_id',
+        'parent_id',
+        'content',
+        'is_approved',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(BlogPost::class, 'blog_post_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(BlogComment::class, 'parent_id');
+    }
+}
