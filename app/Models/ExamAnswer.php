04ba8c3a5ed2b003142e59abbd6f3ffea7b8c3e8
unknown <mdtareq.me@gmail.com>
2025-07-25 01:43:03 +0600

    Exam Answer added

---
+++ app/Models/ExamAnswer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExamAnswer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'exam_attempt_id',
+        'question_id',
+        'selected_option_id',
+        'answer_text',
+        'is_correct',
+        'marks_awarded'
+    ];
+
+    public function attempt()
+    {
+        return $this->belongsTo(ExamAttempt::class, 'exam_attempt_id');
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function selectedOption()
+    {
+        return $this->belongsTo(QuestionOption::class, 'selected_option_id');
+    }
+}
