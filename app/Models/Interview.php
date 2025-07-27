b9bcb015deea3e1d7241d1b558c1687c71eb3ed4
unknown <mdtareq.me@gmail.com>
2025-07-28 01:44:06 +0600

    interview model added

---
+++ app/Models/Interview.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Interview extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'start_time',
+        'end_time',
+        'meeting_url',
+        'status'
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function participants()
+    {
+        return $this->belongsToMany(User::class, 'interview_participants');
+    }
+}
