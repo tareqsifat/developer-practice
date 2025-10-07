61e4a975b11a2b05dd84a0e05bbe9ff90bdf6009
unknown <mdtareq.me@gmail.com>
2025-10-07 21:24:52 +0600

    added routes for auth secure

--- routes/api.php
+++ routes/api.php
@@ -40,13 +40,13 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
-    Route::post('reset-password', [AuthController::class, 'resetPassword']);
-    Route::post('verify-email', [AuthController::class, 'verifyEmail']);
     // Protected Auth Routes
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::get('user', [AuthController::class, 'user']);
         Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('reset-password', [AuthController::class, 'resetPassword']);
+        Route::post('verify-email', [AuthController::class, 'verifyEmail']);
     });
 });
 
