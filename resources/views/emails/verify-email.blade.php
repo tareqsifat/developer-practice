c041ae3914b2381aafd224335cef954d1c776c10
unknown <mdtareq.me@gmail.com>
2025-10-20 01:26:21 +0600

    Varify Email done

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -1,10 +1,14 @@
 <x-mail::message>
 # Hello {{ $user->name }},
 
-Please verify your email by clicking the button below.
+@if ($type == 2)
+    Please verify your email by clicking the button below.
+@elseif($type == 3)
+    {!! '<span class="w-100">We received a request to reset your password. Please click the button below to set a new password.</span>' !!}
+@endif
 
 @component('mail::button', ['url' => url('/verify-email?token=' . $token)])
-Verify Email
+@if ($type == 2) Verify Email @elseif($type == 3) Reset Password @endif
 @endcomponent
 OR Submit the OTP: {{ $otp }} <br/>
 If you didn't initiate this, please ignore this email.
