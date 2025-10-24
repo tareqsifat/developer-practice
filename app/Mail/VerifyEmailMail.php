b487e5b7162b6d2af6290dad3efd29bba8e2d3b7
unknown <mdtareq.me@gmail.com>
2025-10-25 01:21:24 +0600

    Working On Verify Email

--- app/Mail/VerifyEmailMail.php
+++ app/Mail/VerifyEmailMail.php
@@ -3,24 +3,28 @@
 namespace App\Mail;
 
 use App\Models\User;
+use App\Models\VerificationOtp;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Support\Facades\Log;
 
 class VerifyEmailMail extends Mailable
 {
     public $user;
     public $otp;
     public $token;
+    public $type;
 
     /**
      * Create a new message instance.
      */
-    public function __construct(User $user, string $otp, string $token)
+    public function __construct(User $user, string $otp, string $token, string $type)
     {
         $this->user = $user;
         $this->otp = $otp;
         $this->token = $token;
+        $this->type = $type;
     }
 
     /**
@@ -28,8 +32,14 @@ class VerifyEmailMail extends Mailable
      */
     public function envelope(): Envelope
     {
+        Log::info("Type: " . $this->type);
+        if($this->type == VerificationOtp::getTypeCode(VerificationOtp::EMAIL_VERIFICATION)) {
+            $subject = 'Verify Email Mail';
+        } else if($this->type == VerificationOtp::getTypeCode(VerificationOtp::PASSWORD_RESET)){
+            $subject = 'Password Reset Request';
+        }
         return new Envelope(
-            subject: 'Verify Email Mail',
+            subject: $subject,
         );
     }
 
