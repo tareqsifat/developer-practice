5721011ac1a0193c111d383e3c0086e1c41beea5
unknown <mdtareq.me@gmail.com>
2025-11-27 19:12:02 +0600

    added pivot table subject topics

---
+++ database/migrations/2025_11_20_183104_subject_topics.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subject_topic', function (Blueprint $table) {
+            $table->foreignId('subject_id')->constrained()->onDelete('cascade');
+            $table->foreignId('topic_id')->constrained()->onDelete('cascade');
+
+            $table->unique(['subject_id', 'topic_id']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subject_topics');
+    }
+};
